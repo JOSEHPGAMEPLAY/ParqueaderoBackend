0266a0fa07d969957af6fb0c4b3464c4e0006dd4
Jose Vera <jose.hvjhvj@gmail.com>
2024-06-01 11:15:11 -0500

    refactor: :recycle: Correccion de nombre del modelo

--- models/DailyParkingRecord.js
+++ models/DailyParkingRecord.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const dailyParkingRecordSchema = new mongoose.Schema({
-    date: { type: Date, requierd: true },
+    date: { type: Date, required: true },
     parkedCars: [
         { type: mongoose.Schema.Types.ObjectId, ref: "ParkingRecord" },
     ],
     totalEarned: { type: Number, default: 0, required: true },
 });
 
-const DailyParkingRecord = mongoose.model('DailyPakingRecord',dailyParkingRecordSchema);
+const DailyParkingRecord = mongoose.model('DailyParkingRecord',dailyParkingRecordSchema);
 module.exports = DailyParkingRecord;
